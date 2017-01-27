f5bd452672a2a8df5e621988319c3cf1b78195ed
John Dwyer <jgdwyer@gmail.com>
2017-01-26 22:44:08 -0800

    Makes PostgreSQL database from pandas dataframe (runs cln.py too)

---
+++ mkdb.py
@@ -0,0 +1,12 @@
+import pickle
+import cln
+from sqlalchemy import create_engine
+import pandas as pd
+import psycopg2
+
+def store_db():
+    df=pd.read_pickle('./dfs/art_a.pickle')
+    df['date'] = cln.convert_year(df['date'], debug=False)
+    engine = create_engine('postgresql://jgdwyer:1234@localhost/art_1')
+    print(engine.url)
+    df.to_sql('artworks', engine, if_exists='replace')
